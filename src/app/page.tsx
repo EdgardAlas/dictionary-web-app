c123dfa11b9cc375c624776410b92bbc6166b336
Edgard Alas <edgardalasu@gmail.com>
2024-07-18 09:37:57 -0600

    chore: Add @radix-ui/react-select dependency and update header styling in page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Logo from '@/assets/images/logo.svg';
 export default function Home() {
 	return (
 		<>
-			<header className='flex items-center'>
+			<header>
 				<Logo />
 			</header>
 		</>
