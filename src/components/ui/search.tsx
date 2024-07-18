8cb71a6b4aa5bf79aa92568303ea2a3bc4bf9788
Edgard Alas <edgardalasu@gmail.com>
2024-07-18 12:39:26 -0600

    chore: Update search functionality and error handling in Search component

--- src/components/ui/search.tsx
+++ src/components/ui/search.tsx
@@ -1,40 +1,67 @@
 'use client';
 
 import SearchIcon from '@/assets/images/icon-search.svg';
+import { Typography } from '@/components/ui/typography';
+import { cn } from '@/lib/cn';
 import { useParams, useRouter } from 'next/navigation';
-import { useTransition } from 'react';
+import { useState, useTransition } from 'react';
+
+const errorMessage = 'Whoops, can’t be empty…';
 
 export const Search = () => {
 	const router = useRouter();
 	const [pending, startTransition] = useTransition();
+	const [error, setError] = useState<string | null>(null);
 	const params = useParams();
 
 	return (
-		<form
-			className='relative mb-5 md:mb-[2.6875rem]'
-			onSubmit={(e) => {
-				e.preventDefault();
-
-				if (pending) {
-					return;
-				}
-
-				const search = (e.target as HTMLFormElement).search?.value;
-
-				startTransition(() => {
-					if (search) {
-						router.push(`/${search}`);
+		<section className='mb-5 md:mb-[2.6875rem]'>
+			<form
+				className='relative'
+				onSubmit={(e) => {
+					e.preventDefault();
+					if (pending) {
+						return;
 					}
-				});
-			}}
-		>
-			<input
-				defaultValue={params.word?.[0]}
-				name='search'
-				className='h-12 w-full rounded-[1rem] bg-gray-100 py-[0.875rem] pr-14 ps-6 text-base font-bold focus:outline focus:outline-theme-purple-100 dark:bg-theme-black-300 md:h-16 md:py-[1.1875rem] md:text-[1.25rem]'
-				autoFocus
-			/>
-			<SearchIcon className='absolute right-6 top-1/2 -translate-y-1/2' />
-		</form>
+					const search = (e.target as HTMLFormElement).search?.value;
+					if (!search.trim()) {
+						setError(errorMessage);
+						return;
+					}
+					startTransition(() => {
+						if (search) {
+							router.push(`/${search}`);
+						}
+					});
+				}}
+			>
+				<input
+					defaultValue={params.word?.[0]}
+					name='search'
+					onChange={(e) => {
+						if (e.target.value && error) {
+							setError(null);
+						}
+
+						if (!e.target.value) {
+							setError(errorMessage);
+						}
+					}}
+					className={cn(
+						'h-12 w-full rounded-[1rem] bg-gray-100 py-[0.875rem] pr-14 ps-6 text-base font-bold transition-[background-color] focus:outline focus:outline-theme-purple-100 dark:bg-theme-black-300 md:h-16 md:py-[1.1875rem] md:text-[1.25rem]',
+						{
+							'!outline !outline-theme-red-100': error,
+						}
+					)}
+					autoFocus
+				/>
+				<SearchIcon className='absolute right-6 top-1/2 -translate-y-1/2' />
+			</form>
+			{error && (
+				<Typography variant={'heading-s'} className='mt-2 text-theme-red-100'>
+					{error}
+				</Typography>
+			)}
+		</section>
 	);
 };
